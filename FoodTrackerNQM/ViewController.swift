2809eb770f70a14a1740defb1fd9ed6cb1a9aa5b
Nnamdi Massally <nqm14a@lib-301-21-9305.local>
2019-02-25 08:49:51 -0600

    Started work on the ViewController

--- FoodTrackerNQM/ViewController.swift
+++ FoodTrackerNQM/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
