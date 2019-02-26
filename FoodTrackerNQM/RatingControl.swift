b8f3253ec866b43e791d992491d40ec8b3dd83b8
Nnamdi Massally <nqm14a@lib-301-24-9308.local>
2019-02-25 22:39:53 -0600

    Red button not appearing?

---
+++ FoodTrackerNQM/RatingControl.swift
@@ -0,0 +1,51 @@
+//
+//  RatingControl.swift
+//  FoodTrackerNQMTests
+//
+//  Created by Nnamdi Massally on 2/25/19.
+//  Copyright © 2019 Nnamdi Massally. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    //MARK: Initialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtons()
+        print("It ran!")
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButtons()
+    }
+    
+    //MARK: Button Action
+    func ratingButtonTapped(button: UIButton) {
+        print("Button pressed!")
+    }
+    
+    //MARK: Private Methods
+    
+    private func setupButtons() {
+        
+        // Create the button
+        let button = UIButton()
+        button.backgroundColor = UIColor.red
+        
+        // Add constraints
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.weightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        
+        // Setup the button action
+        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+        
+        // Add the button to the stack
+        addArrangedSubview(button)
+    }
+
+}
