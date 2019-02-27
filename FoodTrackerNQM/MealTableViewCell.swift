b51a9d918f9bcc1e61d47f29ae003d6a62e81c41
Nnamdi Massally <nqm14a@lib-301-07-9115.local>
2019-02-26 20:14:21 -0600

    Adding images to TableView

---
+++ FoodTrackerNQM/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTrackerNQM
+//
+//  Created by Nnamdi Massally on 2/26/19.
+//  Copyright © 2019 Nnamdi Massally. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
